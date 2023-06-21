cfb0d18de3e0954d1158e2f7848f7aa652e52bbe
WuYenTing <ap95071@gmail.com>
2023-06-22 00:20:03 +0800

    ajax test 1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function apidata() {
     $.ajax({
-        url:'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-1ED588D9-6FA7-4767-9C73-350D6A7B0401',
+        url:'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-1ED588D9-6FA7-4767-9C73-350D6A7B0401&format=JSON&locationName=&elementName=&sort=time',
         method: "GET",
         datatype:"json",
         success: function(res){
             console.log(res);
-            data = res.records.locations[0].location[lacationnum].weatherElement;
-            data1 = res.records.locations[0].location;
-            console.log(res.records.locations[0].location[lacationnum].locationName);
-            console.log(data);
+            //data = res.records.locations[0].location[lacationnum].weatherElement;
+            //data1 = res.records.locations[0].location;
+            //console.log(res.records.locations[0].location[lacationnum].locationName);
+            //console.log(data);
         }
     });
 }
