d3be180c69ec99e7a3a3718bde2ab86f54f79d9c
WuYenTing <ap95071@gmail.com>
2023-06-22 14:41:47 +0800

    why

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ function apidata() {
         url:'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-1ED588D9-6FA7-4767-9C73-350D6A7B0401&format=JSON&locationName=%E8%87%BA%E5%8D%97%E5%B8%82&elementName=&sort=time',
         method: "GET",
         datatype:"json",
-        success: function(res){
-            console.log(res);
+        success: function(){
+            console.log('why?');
         }
     });
 }
