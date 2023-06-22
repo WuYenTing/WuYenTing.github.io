12d9c5705be73c68806651369944348b6f4e760c
WuYenTing <ap95071@gmail.com>
2023-06-23 02:22:01 +0800

    hey hey

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-function mainstream(){
+apidata();
+function apidata(){
     $.ajax({
-    url:'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-1ED588D9-6FA7-4767-9C73-350D6A7B0401&format=JSON&locationName=%E6%96%B0%E7%AB%B9%E5%B8%82&sort=time',
+    url:'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-1ED588D9-6FA7-4767-9C73-350D6A7B0401&format=JSON&locationName=%E8%87%BA%E5%8C%97%E5%B8%82&elementName=MinT,UVI,MaxT,PoP12h,Wx&sort=time',
     method: "GET",
     datatype:"json",
     success: function(res){
         console.log(res);
+        title = res.records.locations[0];
+        console.log(title);
+        detail = res.records.locations[0].location[0].weatherElement;
+        console.log(detail);
     }           
     });
 };
-mainstream();
+
 ///????
 ///????
