1376bb9fa4e377f089f93b9d6f2f23d8883b124f
WuYenTing <ap95071@gmail.com>
2023-06-22 20:42:13 +0800

    chage api url

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function apidata() {
     $.ajax({
-        url:'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-1ED588D9-6FA7-4767-9C73-350D6A7B0401&format=JSON&locationName=%E8%87%BA%E5%8D%97%E5%B8%82&sort=time',
+        url:'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-930C6111-767C-4BCF-A977-B55FED47B943&format=JSON&locationName=%E6%96%B0%E7%AB%B9%E5%B8%82&sort=time',
         method: "GET",
         datatype: "json",
         success: function(res){
