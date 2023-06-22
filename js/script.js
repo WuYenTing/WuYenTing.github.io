924df708cdd5a41c8eae00c6ceaeb1d2cba3d55d
WuYenTing <ap95071@gmail.com>
2023-06-22 21:42:06 +0800

    }

--- js/script.js
+++ js/script.js
@@ -9,6 +9,6 @@ function mainstream(lacationnum){
         data1 = res.records.locations[0].location;
         console.log(res.records.locations[0].location[lacationnum].locationName);
         console.log(data);
-    }    
+        }    
     });
 };
