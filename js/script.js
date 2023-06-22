13bbe5696ca0bec7a23484207f062ff99116c78b
WuYenTing <ap95071@gmail.com>
2023-06-22 21:49:50 +0800

    ///???/////

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
-function mainstream(lacationnum){
+function mainstream(){
     $.ajax({
     url:'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-1ED588D9-6FA7-4767-9C73-350D6A7B0401&format=JSON&locationName=%E6%96%B0%E7%AB%B9%E5%B8%82&sort=time',
     method: "GET",
     datatype:"json",
     success: function(res){
         console.log(res);
-        data = res.records.locations[0].location[lacationnum].weatherElement;
-        data1 = res.records.locations[0].location;
-        console.log(res.records.locations[0].location[lacationnum].locationName);
-        console.log(data);
         }           
     });
 };
