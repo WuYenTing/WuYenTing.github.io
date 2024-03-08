9ef50c808e9e3a788b599832b9488e0e037d5a2a
WuYenTing <ap95071@gmail.com>
2024-03-08 22:21:34 +0800

    test grid

--- js/script.js
+++ js/script.js
@@ -1,18 +1,64 @@
 apidata();
+var WeatherAPIurl = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-190C6DAA-84DC-4F95-A31D-BA4B80F4CC85"
 function apidata(){
     $.ajax({
-    url:'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-1ED588D9-6FA7-4767-9C73-350D6A7B0401&format=JSON&locationName=%E8%87%BA%E5%8C%97%E5%B8%82&elementName=MinT,UVI,MaxT,PoP12h,Wx&sort=time',
+    url: "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-190C6DAA-84DC-4F95-A31D-BA4B80F4CC85",
     method: "GET",
     datatype:"json",
     success: function(res){
-        console.log(res);
-        title = res.records.locations[0];
-        console.log(title);
-        detail = res.records.locations[0].location[0].weatherElement;
-        console.log(detail);
+        console.log(res.records.location[5]);
+        var TaipeiWeatherElement=res.records.location[5].weatherElement;
+        var Wx = TaipeiWeatherElement[0];
+        var PoP = TaipeiWeatherElement[1];
+        var MinT = TaipeiWeatherElement[2];
+        var CI = TaipeiWeatherElement[3];
+        var MaxT = TaipeiWeatherElement[4];
+        //console.log(Wx);
+        //WeatherDescription(Wx);
+        //console.log(PoP);
+        //console.log(MinT);
+        //console.log(MaxT);
+        //MinAndMaxTempature(MinT, MaxT);
+        //console.log(CI);
+        //FeelingDescription(CI);
+        Prediction(Wx, MinT, MaxT, CI, PoP);
     }           
     });
 };
 
-///????
-///????
+
+function WeatherDescription(Wx){
+    for(let i=0;i<3;i++){
+        var WD = document.querySelector("WD"+i);
+        WD.textContent = Wx.time[i].parameter.parameterName;
+        console.log(Wx.time[i].parameter);
+    }
+}
+function MinAndMaxTempature(MinT, MaxT){
+    for(let i=0;i<3;i++){
+        var MAMT = document.querySelector("MT"+i);
+        MAMT.textContent = MinT.time[i].parameter.parameterName + ' ' +MaxT.time[i].parameter.parameterName;
+        console.log(MinT.time[i].parameter);
+        console.log(MaxT.time[i].parameter);
+    }
+}
+function FeelingDescription(CI){
+    for(let i=0;i<3;i++){
+        var Ci = document.querySelector("CI"+i);
+        Ci.textContent = CI.time[i].parameter.parameterName;
+        console.log(CI.time[i].parameter);
+    }
+}
+
+function Prediction(Wx, MinT, MaxT, CI, PoP){
+    for(let i=0;i<3;i++){
+        var WD = document.querySelector("WD"+i);
+        WD.textContent = Wx.time[i].parameter.parameterName;
+        var MAMT = document.querySelector("MT"+i);
+        MAMT.textContent = MinT.time[i].parameter.parameterName + ' ' +MaxT.time[i].parameter.parameterName;
+        var Ci = document.querySelector("CI"+i);
+        Ci.textContent = CI.time[i].parameter.parameterName;
+        var POP = document.querySelector("POP"+i);
+        POP.textContent = PoP.time[i].parameter.parameterName;
+    }
+}
